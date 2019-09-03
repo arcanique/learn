09d1c451a5faf14a7949eb270bc3c7a7c1d7824c
ang-yi <angyiy@yeah.net>
2019-09-03 14:49:02 +0800

    second commit

---
+++ pkg/dijkstra/dijkstra.go
@@ -0,0 +1,11 @@
+package dijkstra
+
+type Array  struct{
+	data [][]int
+}
+
+func (array *Array)GetShortestpath() {
+
+}
+
+
---
+++ pkg/example/intdivision/intdivision.go
@@ -0,0 +1,8 @@
+package intdivision
+
+
+func division(n,m int) {
+	if m == 1{
+		return
+	}
+}
